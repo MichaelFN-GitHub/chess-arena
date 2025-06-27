54171467155c46f814428e976aed3ee41bbfc6ac
Michael F N <Michael F N>
2025-06-27 19:54:17 +0200

    Deleted unused code in main

--- src/main/java/com/MichaelFN/chess/Main.java
+++ src/main/java/com/MichaelFN/chess/Main.java
@@ -4,15 +4,9 @@ import com.MichaelFN.chess.GUI.GUI;
 import com.MichaelFN.chess.V1.*;
 
 import javax.swing.*;
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        BoardState boardState = new BoardState();
-
-        EngineV1 V1 = new EngineV1();
-
-        SwingUtilities.invokeLater(() -> new GUI(boardState));
+        SwingUtilities.invokeLater(() -> new GUI(new BoardState()));
     }
 }
