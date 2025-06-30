ed6cb7d771f58d2b6d0c5627ffa953ae58696000
Michael F N <Michael F N>
2025-06-30 14:27:52 +0200

    Small refactor

--- src/main/java/com/MichaelFN/chess/Main.java
+++ src/main/java/com/MichaelFN/chess/Main.java
@@ -6,18 +6,11 @@ import com.MichaelFN.chess.v1.*;
 
 import javax.swing.*;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Arena.runTournament(10, 100);
+        //Arena.runTournament(10, 100);
 
-        //SwingUtilities.invokeLater(() -> new GUI(new BoardState()));
+        SwingUtilities.invokeLater(() -> new GUI(new BoardState()));
     }
 }
